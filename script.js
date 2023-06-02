64842b377d086051c7e27cb476b46d7a992a0976
Brian Guintos Recinos <brianguintos@gmail.com>
2023-06-02 14:19:19 -0700

    got mole on the hole styled

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var timeLeft = 60;
 function gameOver() {
   cancelInterval(timer);
 
-  $("#playAgain").show();
+  // $("#playAgain").show();
 }
 
 function updateTimer() {
@@ -25,3 +25,49 @@ function start() {
   $("playAgain").hide();
 }
 
+
+
+
+
+
+
+
+const moleElements = document.querySelectorAll('.moles');
+
+function handleWhack() {
+  this.parentNode.classList.remove('active');
+  updateScore();
+}
+
+moleElements.forEach((moleELement) => {
+  moleELement.addEventListener('click', handleWhack);
+});
+
+function showMole() {
+  const randomMoleIndex = Math.floor(Math.random() * moleElements.length);
+  const moleELement = moleElements[randomMoleIndex].parentNode;
+
+  moleELement.classList.add('active');
+
+  setTimeout(() => {
+    moleELement.classList.remove('active');
+  }, 1000);
+}
+
+let intervalId;
+
+function start(); {
+  resetScore();
+  intervalId = setInterval(showMole, 2000);
+}
+
+function resetScore() {
+  const scoreElement = document.querySelector('.score');
+  let currentScore = parseInt(scoreElement.textContent);
+  currentScore++;
+
+  scoreElement.textContent = currentScore.toString().padStart(2, '0');
+}
+
+const startButton = document.getElementById('start');
+startButton.addEventListener('click', start);
