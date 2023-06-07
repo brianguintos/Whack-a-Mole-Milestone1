962c58a1dc56fd49a743d0b53780f1b263df2a4c
Brian Guintos Recinos <brianguintos@gmail.com>
2023-06-07 14:56:10 -0700

    got rid of unnecessary code and images no longer used

--- script.js
+++ script.js
@@ -1,89 +1,84 @@
-    // jquery function that waits for DOM to finish loading before starting functions in brackets
-    $(document).ready(function () {
-      var timer;
-      var timeLeft = 60;
-      let score = 0; // Variable to store the score
+// jquery function that waits for DOM to finish loading before starting functions in brackets
+$(document).ready(function () {
+  var timer;
+  var timeLeft = 60;
+  let score = 0; // Variable to store the score
 
+  //when game ends this will clear timer and stop showing moles
+  function endGame() {
+    clearInterval(timer);
+    clearInterval(intervalMole);
+    updateTimer();
+  }
 
-      //when game ends this will clear timer 
-      function endGame() {
-        clearInterval(timer);
-        clearInterval(intervalMole);
-        updateTimer();
-      }
+  // used help creating the timer. Gave credit in README file
+  function updateTimer() {
+    //responsible for subtracting time left by 1
+    timeLeft = timeLeft - 1;
+    if (timeLeft >= 0) {
+      //time left greater than or equal to 0 then show time
+      console.log(timeLeft);
+      $("#timer").html(timeLeft);
+    } else {
+      endGame(); //if time goes negative then game ends
+    }
+  }
 
-// usd help creating the timer. Gave credit in README file
-      function updateTimer() { //responsible for subtracting time left by 1
-        timeLeft = timeLeft - 1;
-        if (timeLeft >= 0) { //time left greater than or equal to 0 then show time
-          console.log(timeLeft);
-          $("#timer").html(timeLeft);
-        } else {
-          endGame();//if time goes negative then game ends
-        }
-      }
+  function start() {
+    timer = setInterval(updateTimer, 1000); //update timer by the second.
+    resetScore(); // begins with the score at 0
+    intervalMole = setInterval(showMole, 2000); //a new mole will pop out every 2 seconds
+  }
 
+  const moleElements = $(".moles"); //this gets all the elements with the specified class of .moles and stores them in the moleElements variable
 
-      function start() {
-        timer = setInterval(updateTimer, 1000); //update timer by the second.
-        resetScore(); // begins with the score at 0
-        intervalMole = setInterval(showMole, 2000); //a new mole will pop out every 2 seconds
-      }
+  //when you click on the moles this will give you a point. If you click on anything else it won't
+  function handleWhack() {
+    const moleHole = $(this).parent(); //to retrieve the parent element moleHole
+    if (moleHole.hasClass("active")) {
+      //if you whack a mole then it will give you a class of active
+      moleHole.removeClass("active"); //goes on to remove the mole once you've whacked it
+      incrementScore(); //Increase the score when a mole is whacked
+    }
+  }
 
-      const moleElements = $(".moles"); //this gets all the elements with the specified class of .moles and stores them in the moleElements variable  
+  function incrementScore() {
+    score += 1; // Increment the score by 1
+    updateScoreDisplay(); // Update the score display
+  }
 
-//when you click on the moles this will give you a point. If you click on anything else it won't
-      function handleWhack() {
-        const moleHole = $(this).parent(); //to retrieve the parent element moleHole
-        if (moleHole.hasClass("active")) { //if you whack a mole then it will give you a class of active
-          moleHole.removeClass("active"); //goes on to remove the mole once you've whacked it
-          incrementScore(); //Increase the score when a mole is whacked
-        }
-      }
+  function updateScoreDisplay() {
+    const scoreElement = $(".score"); // $(".score") will show the current score
+    scoreElement.text(score.toString().padStart(2, "0")); // used to update the current score
+  }
 
+  function resetScore() {
+    score = 0; // starts the score at 0 and updates it when you whack a mole
+    updateScoreDisplay();
+  }
 
-      function incrementScore() {
-        score += 1; // Increment the score by 1
-        updateScoreDisplay(); // Update the score display
-      }
+  moleElements.on("click", handleWhack); //event listener
 
-// to update the score display. $(".score") will show the current score
-      function updateScoreDisplay() {
-        const scoreElement = $(".score");
-        scoreElement.text(score.toString().padStart(2, "0")); // used to update the current score
-      }
+  let activeMole = null; //to keep track of the active mole in the game
 
+  //to make moles pop out of random holes using math.floor and math.random
+  function showMole() {
+    const randomMoleIndex = Math.floor(Math.random() * moleElements.length);
+    const moleElement = moleElements.eq(randomMoleIndex).parent();
 
-      function resetScore() {
-        score = 0; // starts the score at 0 and updates it when you whack a mole
-        updateScoreDisplay();
-      }
+    if (activeMole) {
+      activeMole.removeClass("active");
+    }
 
-      moleElements.on("click", handleWhack); //event listener 
+    moleElement.addClass("active");
+    activeMole = moleElement;
 
+    setTimeout(function () {
+      moleElement.removeClass("active");
+    }, 1000);
+  }
 
-      let activeMole = null; //to keep track of the active mole in the game
+  let intervalMole;
 
-      //to make moles pop out of random holes using math.floor and math.random
-      function showMole() {
-        const randomMoleIndex = Math.floor(
-          Math.random() * moleElements.length
-        );
-        const moleElement = moleElements.eq(randomMoleIndex).parent();
-
-        if (activeMole) {
-          activeMole.removeClass("active");
-        }
-
-        moleElement.addClass("active");
-        activeMole = moleElement;
-
-        setTimeout(function () {
-          moleElement.removeClass("active");
-        }, 1000);
-      }
-
-      let intervalMole; 
-
-      $("#start").on("click", start);
-    });
+  $("#start").on("click", start);
+});
